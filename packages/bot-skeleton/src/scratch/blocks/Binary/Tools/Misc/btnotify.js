b7758a80f47d1c05d7a4a3baf42d2dc3807fcc15
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-03-23 20:04:46 +0000

    b

--- packages/bot-skeleton/src/scratch/blocks/Binary/Tools/Misc/btnotify.js
+++ packages/bot-skeleton/src/scratch/blocks/Binary/Tools/Misc/btnotify.js
@@ -72,6 +72,6 @@ Blockly.JavaScript.btnotify = block => {
         Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC) ||
         `"${localize('<empty message>')}"`;
 
-    const code = `Bot.btnotify({ className: 'journal__text--${notificationType}', message: ${message}, sound: '${sound}', block_id: '${block.id}', variable_name: '${variable_name}' });\n`;
+    const code = `Bot.notify({ className: 'journal__text--${notificationType}', message: ${message}, sound: '${sound}', block_id: '${block.id}', variable_name: '${variable_name}' });\n`;
     return code;
 };

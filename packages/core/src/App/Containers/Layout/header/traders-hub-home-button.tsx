a8123e5efe93cacd8ebfced53df2ca78e2c3ebf0
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-03-25 08:44:34 +0000

    n

--- packages/core/src/App/Containers/Layout/header/traders-hub-home-button.tsx
+++ packages/core/src/App/Containers/Layout/header/traders-hub-home-button.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useHistory, useLocation } from 'react-router';
-import classNames from 'classnames';
 import { Icon, Text } from '@deriv/components';
 import { routes } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
@@ -10,32 +8,24 @@ import { useFeatureFlags } from '@deriv/hooks';
 const TradersHubHomeButton = observer(() => {
     const { ui } = useStore();
     const { is_dark_mode_on } = ui;
-    const history = useHistory();
-    const location = useLocation();
-    const { pathname } = location;
     const { is_next_wallet_enabled, is_next_tradershub_enabled } = useFeatureFlags();
 
-    const redirect_routes = () => {
+    const redirectRoute = () => {
         if (is_next_wallet_enabled) {
             return routes.wallets;
         } else if (is_next_tradershub_enabled) {
-            return 'https://block.binarytool.site'; // Update this line to redirect to the desired URL
+            return 'https://block.binarytool.site'; // Redirect to the desired URL
         }
 
         return routes.traders_hub;
     };
 
+    const handleClick = () => {
+        window.location.href = redirectRoute(); // Perform redirection
+    };
+
     return (
-        <div
-            data-testid='dt_traders_hub_home_button'
-            className={classNames('traders-hub-header__tradershub', {
-                'traders-hub-header__tradershub--active':
-                    pathname === routes.traders_hub ||
-                    pathname === routes.traders_hub_v2 ||
-                    pathname === routes.wallets,
-            })}
-            onClick={() => history.push(redirect_routes())} // This line handles redirection
-        >
+        <div className='traders-hub-header__tradershub' onClick={handleClick}>
             <div className='traders-hub-header__tradershub--home-logo'>
                 <Icon
                     icon={is_dark_mode_on ? 'IcAppstoreHomeDark' : 'IcAppstoreTradersHubHome'}
@@ -43,7 +33,7 @@ const TradersHubHomeButton = observer(() => {
                 />
             </div>
             <Text className='traders-hub-header__tradershub--text'>
-                <Localize i18n_default_text="Binarytool" />
+                <Localize i18n_default_text="Trader's Hub" />
             </Text>
         </div>
     );

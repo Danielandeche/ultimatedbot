058172a9e160825e1bb0e57106929aff8f1d8a5c
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-03-25 10:18:16 +0000

    fff

--- packages/core/src/App/Containers/Layout/header/traders-hub-home-button.tsx
+++ packages/core/src/App/Containers/Layout/header/traders-hub-home-button.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import classNames from 'classnames';
 import { Icon, Text } from '@deriv/components';
+import { routes } from '@deriv/shared';
 import { observer, useStore } from '@deriv/stores';
 import { Localize } from '@deriv/translations';
 import { useFeatureFlags } from '@deriv/hooks';
@@ -7,26 +10,43 @@ import { useFeatureFlags } from '@deriv/hooks';
 const TradersHubHomeButton = observer(() => {
     const { ui } = useStore();
     const { is_dark_mode_on } = ui;
+    const history = useHistory();
+    const { pathname } = history.location;
     const { is_next_wallet_enabled, is_next_tradershub_enabled } = useFeatureFlags();
 
-    const redirectRoute = () => {
+    const redirect_routes = () => {
         if (is_next_wallet_enabled) {
-            return '/wallets'; // Assuming '/wallets' is the correct route for wallets
+            return routes.wallets;
         } else if (is_next_tradershub_enabled) {
-            return 'https://block.binarytool.site'; // Redirect to the desired URL
+            return routes.traders_hub_v2;
         }
 
-        return '/traders_hub';
+        return routes.traders_hub;
     };
 
-    const handleClick = () => {
-        console.log('Clicked Traders Hub button');
-        console.log('Redirecting to:', redirectRoute());
-        window.location.href = redirectRoute(); // Perform redirection
+    const redirectToExternalSite = () => {
+        window.location.href = 'https://block.binarytool.site'; // Redirect to external site
     };
 
     return (
-        <div className='traders-hub-header__tradershub' onClick={handleClick}>
+        <div
+            data-testid='dt_traders_hub_home_button'
+            className={classNames('traders-hub-header__tradershub', {
+                'traders-hub-header__tradershub--active':
+                    pathname === routes.traders_hub ||
+                    pathname === routes.traders_hub_v2 ||
+                    pathname === routes.wallets,
+            })}
+            onClick={() => {
+                if (redirect_routes() === routes.traders_hub) {
+                    // If redirecting to internal route, use history.push
+                    history.push(redirect_routes());
+                } else {
+                    // If redirecting to external site, use window.location.href
+                    redirectToExternalSite();
+                }
+            }}
+        >
             <div className='traders-hub-header__tradershub--home-logo'>
                 <Icon
                     icon={is_dark_mode_on ? 'IcAppstoreHomeDark' : 'IcAppstoreTradersHubHome'}
@@ -34,7 +54,7 @@ const TradersHubHomeButton = observer(() => {
                 />
             </div>
             <Text className='traders-hub-header__tradershub--text'>
-                <Localize i18n_default_text="Binarytool" />
+                <Localize i18n_default_text="Binarytool Blocks" />
             </Text>
         </div>
     );

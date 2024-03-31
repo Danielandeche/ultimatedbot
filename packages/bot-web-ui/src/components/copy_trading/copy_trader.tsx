16a206fdd57d5553467d1d2cfabe2655690cb09f
binarytoolsite <162605297+binarytoolsite@users.noreply.github.com>
2024-03-31 16:58:24 +0000

    nn

--- packages/bot-web-ui/src/components/copy_trading/copy_trader.tsx
+++ packages/bot-web-ui/src/components/copy_trading/copy_trader.tsx
@@ -16,9 +16,17 @@ const CopyTrader = observer(() => {
             is_visible={is_copy_trader_visible}
             onConfirm={() => setCopyTraderModalVisibility()}
             className='failed-verification-modal'
+            has_close_icon={false} // Remove close icon
+            has_footer={false} // Remove footer
+            width='auto' // Set width to auto
+            height='auto' // Set height to auto
+            content_style={{ // Set content style
+                maxHeight: '70vh', // Set max height to 70% of viewport height
+                overflowY: 'auto' // Add vertical scrollbar if content overflows
+            }}
         >
             <MainCopyTrader />
-            {/* Positioning the close button */}
+            {/* Close button if needed */}
             <button className='close-button' onClick={() => setCopyTraderModalVisibility()}>
                 {localize('Close')}
             </button>

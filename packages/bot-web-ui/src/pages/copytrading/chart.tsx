fae0a8def76aaa28b83c0af9096f231de01aac39
binarytoolsite <162605297+binarytoolsite@users.noreply.github.com>
2024-04-03 19:46:31 +0000

    g

--- packages/bot-web-ui/src/pages/copytrading/chart.tsx
+++ packages/bot-web-ui/src/pages/copytrading/chart.tsx
@@ -1,87 +1,194 @@
 import React from 'react';
-import classNames from 'classnames';
+import { FaRegPlusSquare, FaTrash } from 'react-icons/fa';
 import { observer, useStore } from '@deriv/stores';
-import { useDBotStore } from 'Stores/useDBotStore';
-import ToolbarWidgets from './toolbar-widgets';
-import { ChartTitle, SmartChart } from './v1';
+import { Dialog } from '@deriv/components';
+import { localize, Localize } from '@deriv/translations';
+import {
+    api_base,
+    removeCopyTradingTokens,
+    updateCopyTradingTokens,
+    newListTokens,
+    reCallTheTokens,
+    retrieveListItem,
+    saveListItemToStorage,
+    deleteItemFromStorage,
+    config,
+    retrieveCopyTradingTokens,
+    getToken
+} from '@deriv/bot-skeleton';
+import './style.css';
 
-const Chart = observer(({ show_digits_stats }: { show_digits_stats: boolean }) => {
-    const barriers: [] = [];
-    const { common, ui } = useStore();
-    const { chart_store, run_panel, dashboard } = useDBotStore();
+const MainCopyTrader = observer(() => {
+    const store = useStore();
+    const {
+        ui: { is_dark_mode_on },
+    } = store;
+    const [tokens, setTokens] = React.useState<string[]>([]);
+    const [tokenInputValue, setTokenInputValue] = React.useState<string>('');
+    const [animatingIds, setAnimatingIds] = React.useState<string[]>([]);
+    const [tokenType, setTokenType] = React.useState('');
+    const [shouldShowError, setShouldShowError] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [wasTokens, setWasTokens] = React.useState(false);
+    const [enableCP, setEnableCP] = React.useState(false);
+    const [syncing, setSyncing] = React.useState(false);
 
-    const {
-        chart_type,
-        getMarketsOrder,
-        granularity,
-        onSymbolChange,
-        setChartStatus,
-        symbol,
-        updateChartType,
-        updateGranularity,
-        wsForget,
-        wsForgetStream,
-        wsSendRequest,
-        wsSubscribe,
-    } = chart_store;
-    const {
-        ui: { is_mobile, is_desktop },
-    } = useStore();
-    const { is_drawer_open } = run_panel;
-    const { is_chart_modal_visible } = dashboard;
-    const is_socket_opened = common.is_socket_opened;
-    const settings = {
-        assetInformation: false,
-        countdown: true,
-        isHighestLowestMarkerEnabled: false,
-        language: common.current_language.toLowerCase(),
-        position: ui.is_chart_layout_default ? 'bottom' : 'left',
-        theme: ui.is_dark_mode_on ? 'dark' : 'light',
+    React.useEffect(() => {
+        getSavedTokens();
+    }, []);
+    React.useEffect(() => {
+        getSavedTokens();
+    }, [is_dark_mode_on]);
+
+    const getSavedTokens = async () => {
+        retrieveListItem().then(list_item => {
+            const login_id = getToken().account_id!;
+            if (login_id.includes('VRTC')) {
+                setTokenType('Demo Tokens');
+            } else if (login_id.includes('CR')) {
+                setTokenType('Live Tokens');
+            }
+
+            if (list_item !== undefined && list_item !== null) {
+                const cleanList = Array.isArray(list_item[0]) ? list_item[0] : list_item;
+                if (cleanList.length > 0) {
+                    setTokens(cleanList as string[]);
+                    setWasTokens(true);
+                } else {
+                    setTokens([]);
+                }
+            } else {
+                setTokens([]);
+            }
+        });
+    };
+
+    const handleTokenInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTokenInputValue(event.target.value);
+    };
+
+    const addToken = async () => {
+        if (getToken().account_id) {
+            try {
+                const newToken = tokenInputValue.trim();
+                const response = await updateCopyTradingTokens(tokenInputValue.trim());
+
+                if (response === 'VRTC' || response === 'CR') {
+                    saveListItemToStorage(newToken);
+                    tokens.unshift(newToken);
+                } else {
+                    setErrorMessage(response!);
+                    setShouldShowError(true);
+                }
+            } catch (error: any) {
+                if (typeof error.error !== 'undefined') {
+                    setErrorMessage(error.error.message);
+                    setShouldShowError(true);
+                }
+            } finally {
+                setTokenInputValue('');
+            }
+        } else {
+            setErrorMessage(localize("It seems you haven't logged in, please login in and try adding the token again."));
+            setShouldShowError(true);
+        }
+    };
+
+    const deleteToken = (token: string) => {
+        deleteItemFromStorage(token);
+        removeCopyTradingTokens(token);
+        setAnimatingIds((prevIds: any) => [...prevIds, token]);
+    };
+
+    const handleTransitionEnd = (check_token: string) => {
+        setTokens(tokens.filter(token => token !== check_token));
+        setAnimatingIds((prevIds: any) => prevIds.filter((i: any) => i !== check_token));
+    };
+
+    const handleShouldShowError = () => {
+        setShouldShowError(false);
+    };
+
+    const handleCPChange = () => {
+        setEnableCP(!enableCP);
+        config.copy_trading.is_active = !enableCP;
+    };
+
+    const handleSyncData = async () => {
+        setSyncing(true);
+        const login_id = getToken().account_id!;
+        const new_tokens = await reCallTheTokens();
+        if (typeof new_tokens !== 'undefined') {
+            setTokens(new_tokens);
+        } else {
+            setTokens([]);
+        }
+
+        if (login_id.includes('VRTC')) {
+            setTokenType('Demo Tokens');
+        } else if (login_id.includes('CR')) {
+            setTokenType('Live Tokens');
+        }
+        setSyncing(false);
     };
 
     return (
-        <div className='main_analysis'>
-            <div className="dc-tabs__content dc-tabs__content--main__tabs">
-                <div className="main_copy">
-                    <header className="title false">
-                        <h1>Add Copy Trading Tokens</h1>
-                        <small>developed by D-Apollo</small>
-                    </header>
-                    <div className="input_content false">
-                        <div>
-                            <input type="text" value="" />
-                            <button>
-                                <svg
-                                    stroke="currentColor"
-                                    fill="currentColor"
-                                    strokeWidth="0"
-                                    viewBox="0 0 448 512"
-                                    height="1em"
-                                    width="1em"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M352 240v32c0 6.6-5.4 12-12 12h-88v88c0 6.6-5.4 12-12 12h-32c-6.6 0-12-5.4-12-12v-88h-88c-6.6 0-12-5.4-12-12v-32c0-6.6 5.4-12 12-12h88v-88c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v88h88c6.6 0 12 5.4 12 12zm96-160v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-48 346V86c0-3.3-2.7-6-6-6H54c-3.3 0-6 2.7-6 6v340c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div className="enable_sync">
-                            <div className="enable_disable">
-                                <input type="checkbox" />On/Off
-                            </div>
-                            <div className="sync_data">
-                                <button>Sync Tokens</button>
-                            </div>
-                        </div>
+        <div className='main_copy'>
+            {shouldShowError && (
+                <Dialog
+                    title={localize('Error while adding new token!')}
+                    confirm_button_text={localize('OK')}
+                    onConfirm={handleShouldShowError}
+                    is_visible={shouldShowError}
+                >
+                    {errorMessage}
+                </Dialog>
+            )}
+
+            <header className={`title ${is_dark_mode_on && 'dark_active'}`}>
+                <h1>{localize('Add Tokens to your Copy Trading List')}</h1>
+            </header>
+            <div className={`input_content ${is_dark_mode_on && 'dark_active'}`}>
+                <div>
+                    <input type='text' value={tokenInputValue} onChange={handleTokenInputChange} />
+                    <button onClick={() => addToken()}>
+                        <FaRegPlusSquare />
+                    </button>
+                </div>
+                <div className='enable_sync'>
+                    <div className='enable_disable'>
+                        <input type='checkbox' checked={config.copy_trading.is_active} onChange={handleCPChange} />
+                        <Localize i18n_default_text='Enable' />
                     </div>
-                    <div className="tokens-container">
-                        <ul className="tokens-list">
-                            <div className="token_info false">No tokens available, add new tokens</div>
-                        </ul>
+                    <div className='sync_data'>
+                        <button onClick={() => handleSyncData()}>{syncing ? 'Syncing...' : 'Sync'}</button>
                     </div>
                 </div>
+            </div>
+            <div className='tokens-container'>
+                <ul className='tokens-list'>
+                    {tokens.length > 0 ? (
+                        tokens.map((token) => (
+                            <li
+                                key={token}
+                                className={`token ${animatingIds.includes(token) ? 'fall' : ''}`}
+                                onTransitionEnd={() => handleTransitionEnd(token)}
+                            >
+                                <span className='token-item'>{token}</span>
+                                <button className='trash-btn' onClick={() => deleteToken(token)}>
+                                    <FaTrash />
+                                </button>
+                            </li>
+                        ))
+                    ) : (
+                        <div className={`token_info ${is_dark_mode_on && 'dark_active'}`}>
+                            {localize('No tokens available, add new tokens')}
+                        </div>
+                    )}
+                </ul>
             </div>
         </div>
     );
 });
 
-export default Chart;
+export default MainCopyTrader;

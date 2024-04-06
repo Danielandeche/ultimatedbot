43f14a1fec93f004ef4b98627eb1935bd775c818
binarytoolsite <162605297+binarytoolsite@users.noreply.github.com>
2024-04-06 14:47:02 +0000

    vbjnl

--- packages/shared/src/utils/config/config.ts
+++ packages/shared/src/utils/config/config.ts
@@ -15,7 +15,9 @@ export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7';
 
 export const domain_app_ids = {
     // these domains as supported "production domains"
-    'app.binarytool.site/bot': 52960,
+    'deriv.app': 16929, // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'app.deriv.com': 16929,
+    'app.binarytool.site': 52960,
     'staging-app.deriv.com': 16303,
     'app.deriv.me': 1411,
     'staging-app.deriv.me': 1411, // TODO: setup staging for deriv.me
@@ -46,7 +48,6 @@ export const isLocal = () => /localhost(:\d+)?$/i.test(window.location.hostname)
 
 export const getAppId = () => {
     let app_id = null;
-    // '52960'
     const user_app_id = '52960';
     const config_app_id = window.localStorage.getItem('config.app_id');
     const current_domain = getCurrentProductionDomain() || '';
@@ -68,7 +69,7 @@ export const getAppId = () => {
         app_id = 36300;
     } else {
         window.localStorage.removeItem('config.default_app_id');
-        app_id = is_bot ? 19111 : domain_app_ids[current_domain as keyof typeof domain_app_ids] || 52960;
+        app_id = is_bot ? 19111 : domain_app_ids[current_domain as keyof typeof domain_app_ids] || 16929;
     }
 
     return app_id;

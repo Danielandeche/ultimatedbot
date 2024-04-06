8c090c29c860af32e6118ce567f791a299726e04
binarytoolsite <162605297+binarytoolsite@users.noreply.github.com>
2024-04-06 09:21:48 +0000

    sdflgh;

--- packages/core/src/App/Constants/platform-config.ts
+++ packages/core/src/App/Constants/platform-config.ts
@@ -1,37 +1,84 @@
-import { getPlatformSettings, routes } from '@deriv/shared';
+import { getUrlBinaryBot, getUrlSmartTrader, getPlatformSettingsAppstore, routes, getStaticUrl } from '@deriv/shared';
 import { localize } from '@deriv/translations';
+import { PlatformIcons } from 'Assets/svgs/trading-platform';
+import { TAccountCategory, TRegionAvailability } from 'Types';
 
-type TPlatformConfig = {
-    description: () => string;
-    href?: string;
-    icon: string;
-    link_to?: string;
+export type AccountType = { text: 'Real' | 'Demo'; value: TAccountCategory };
+export type RegionAvailability = 'Non-EU' | 'EU' | 'All';
+export const getAccountTypes = (): AccountType[] => [
+    { text: localize('Demo'), value: 'demo' },
+    { text: localize('Real'), value: 'real' },
+];
+export const region_availability: RegionAvailability[] = ['Non-EU', 'EU'];
+
+export type BrandConfig = {
     name: string;
-    title: () => string;
+    icon: keyof typeof PlatformIcons;
+    availability: TRegionAvailability;
+    is_deriv_platform?: boolean;
 };
 
-const platform_config: TPlatformConfig[] = [
+export interface PlatformConfig {
+    name: string;
+    app_desc: string;
+    link_to?: string;
+    is_external?: boolean;
+    new_tab?: boolean;
+}
+
+export interface MfPlatformConfig extends PlatformConfig {
+    app_icon: string;
+    app_title: string;
+}
+
+export const getAppstorePlatforms = (): PlatformConfig[] => [
     {
-        icon: getPlatformSettings('trader').icon,
-        title: () => getPlatformSettings('trader').name,
-        name: getPlatformSettings('trader').name,
-        description: () => localize('Binarytool Trader.'),
+        name: getPlatformSettingsAppstore('trader').name,
+        app_desc: localize('Options and multipliers trading platform.'),
         link_to: routes.trade,
     },
     {
-        icon: getPlatformSettings('dbot').icon,
-        title: () => getPlatformSettings('dbot').name,
-        name: getPlatformSettings('dbot').name,
-        description: () => localize('Binarytool DBOT.'),
+        name: getPlatformSettingsAppstore('dbot').name,
+        app_desc: localize('Automate your trading, no coding needed.'),
         link_to: routes.bot,
+        is_external: true,
     },
     {
-        icon: getPlatformSettings('smarttrader').icon,
-        title: () => getPlatformSettings('smarttrader').name,
-        name: getPlatformSettings('smarttrader').name,
-        description: () => localize('Binarytool Blocks'),
-        href: 'https://block.binarytool.site', 
-    }
+        name: getPlatformSettingsAppstore('smarttrader').name,
+        app_desc: localize('Our legacy options trading platform.'),
+        link_to: "https://binarytool.site/robot.html",
+        is_external: true,
+    },
+    {
+        name: getPlatformSettingsAppstore('bbot').name,
+        app_desc: localize('Our legacy automated trading platform.'),
+        link_to: "https://block.binarytool.site/",
+        is_external: true
+    }    
+    {
+        name: getPlatformSettingsAppstore('go').name,
+        app_desc: localize('Trade on the go with our mobile app.'),
+        link_to: getStaticUrl('/deriv-go'),
+        is_external: true,
+        new_tab: true,
+    },
 ];
 
-export default platform_config;
+export const getMFAppstorePlatforms = (): MfPlatformConfig[] => [
+    {
+        app_icon: getPlatformSettingsAppstore('trader').icon,
+        app_title: getPlatformSettingsAppstore('trader').name,
+        name: getPlatformSettingsAppstore('trader').name,
+        app_desc: localize('Multipliers trading platform.'),
+        link_to: routes.trade,
+    },
+];
+
+// The platform names were taken from packages/shared/brand.config.json
+export const DERIV_PLATFORM_NAMES = {
+    TRADER: 'Binarytool Trader',
+    DBOT: 'Binarytool DBot',
+    SMARTTRADER: 'SmartTrader',
+    BBOT: 'Binary Bot',
+    GO: 'Deriv GO',
+} as const;

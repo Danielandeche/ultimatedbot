2ce3045b6c3f5e9692b5996a152f4f614db1d8bf
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-09-06 08:22:59 +0000

    loading

--- packages/components/src/components/loading/loading.tsx
+++ packages/components/src/components/loading/loading.tsx
@@ -11,39 +11,7 @@ export type TLoadingProps = React.HTMLProps<HTMLDivElement> & {
 };
 
 const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status, theme }: Partial<TLoadingProps>) => {
-    const [speed, setSpeed] = useState<string>('Checking speed...');
     const theme_class = theme ? `barspinner-${theme}` : 'barspinner-light';
-
-    // Function to check internet speed
-    const checkInternetSpeed = (callback: (speed: number) => void) => {
-        const image = new Image();
-        const startTime = new Date().getTime();
-        const imageUrl = 'https://example.com/large-image.jpg'; // Replace with a URL to a large file
-
-        image.onload = () => {
-            const endTime = new Date().getTime();
-            const duration = (endTime - startTime) / 1000; // time in seconds
-            const fileSize = 5000000; // Size of the file in bytes (replace with actual size)
-
-            const speed = fileSize / duration / 1024 / 1024; // Speed in Mbps
-            callback(speed);
-        };
-
-        image.onerror = () => {
-            callback(0);
-        };
-
-        image.src = `${imageUrl}?cacheBuster=${new Date().getTime()}`;
-    };
-
-    // Update speed state on component mount
-    useEffect(() => {
-        checkInternetSpeed(speed => {
-            const speedText = speed > 0 ? `${speed.toFixed(2)} Mbps` : 'Unable to determine speed';
-            setSpeed(`Internet Speed: ${speedText}`);
-        });
-    }, []);
-
     return (
         <div
             data-testid='dt_initial_loader'
@@ -56,13 +24,14 @@ const Loading = ({ className, id, is_fullscreen = true, is_slow_loading, status,
             )}
         >
             <div id={id} className={classNames('initial-loader__barspinner', 'barspinner', theme_class)}>
-                <div className='container-loader'>
-                    <div className='loader'>
-                        Loading
-                        <span />
+                <div className='container'>
+                    <div className='container-loader'>
+                        <div className='loaderb'>
+                            Loading...
+                            <span />
+                        </div>
+                        <div className='text-loader'>app.binarytool.site/bot</div>
                     </div>
-                    <div className='text-loader'>app.binarytool.site/bot</div>
-                    <div id='speed-indicator'>{speed}</div>
                 </div>
             </div>
             {is_slow_loading &&

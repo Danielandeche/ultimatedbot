deb8cc293110b960017f22ead29120713aaafb07
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-09-19 11:57:53 +0000

    rebranding

--- packages/core/src/App/Constants/platform-config.ts
+++ packages/core/src/App/Constants/platform-config.ts
@@ -1,3 +1,4 @@
+
 import { getPlatformSettings, routes } from '@deriv/shared';
 import { localize } from '@deriv/translations';
 
@@ -6,32 +7,62 @@ type TPlatformConfig = {
     href?: string;
     icon: string;
     link_to?: string;
-    name: string;
-    title: () => string;
 };
 
 const platform_config: TPlatformConfig[] = [
     {
         icon: 'IcRebrandingDerivTrader',
-        title: () => getPlatformSettings('trader').name,
-        name: getPlatformSettings('trader').name,
         description: () => localize('Ultimate D-Trader.'),
         link_to: routes.trade,
     },
     {
         icon: 'IcRebrandingDerivBot',
-        title: () => getPlatformSettings('dbot').name,
-        name: getPlatformSettings('dbot').name,
         description: () => localize('Ultimate DBOT.'),
         link_to: routes.bot,
     },
     {
         icon: 'IcRebrandingBinaryBot',
-        title: () => getPlatformSettings('smarttrader').name,
-        name: getPlatformSettings('smarttrader').name,
         description: () => localize('Ultimate Bot'),
         href: 'https://bot.ultimatedigits.ai/', 
     }
 ];
 
 export default platform_config;
+
+// import { getPlatformSettings, routes } from '@deriv/shared';
+// import { localize } from '@deriv/translations';
+
+// type TPlatformConfig = {
+//     description: () => string;
+//     href?: string;
+//     icon: string;
+//     link_to?: string;
+//     name: string;
+//     title: () => string;
+// };
+
+// const platform_config: TPlatformConfig[] = [
+//     {
+//         icon: 'IcRebrandingDerivTrader',
+//         title: () => getPlatformSettings('trader').name,
+//         name: getPlatformSettings('trader').name,
+//         description: () => localize('Ultimate D-Trader.'),
+//         link_to: routes.trade,
+//     },
+//     {
+//         icon: 'IcRebrandingDerivBot',
+//         title: () => getPlatformSettings('dbot').name,
+//         name: getPlatformSettings('dbot').name,
+//         description: () => localize('Ultimate DBOT.'),
+//         link_to: routes.bot,
+//     },
+//     {
+//         icon: 'IcRebrandingBinaryBot',
+//         title: () => getPlatformSettings('smarttrader').name,
+//         name: getPlatformSettings('smarttrader').name,
+//         description: () => localize('Ultimate Bot'),
+//         href: 'https://bot.ultimatedigits.ai/', 
+//     }
+// ];
+
+// export default platform_config;

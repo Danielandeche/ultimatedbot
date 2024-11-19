e76f6212e438543c3bb37438c0cca3703b11cc42
Daniel Andeche <139417311+Danielandeche@users.noreply.github.com>
2024-11-19 19:42:38 +0000

    ldp

--- packages/bot-web-ui/src/pages/analysistool/chart.tsx
+++ packages/bot-web-ui/src/pages/analysistool/chart.tsx
@@ -2,43 +2,17 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 import { observer, useStore } from '@deriv/stores';
 import { useDBotStore } from 'Stores/useDBotStore';
-import ToolbarWidgets from './toolbar-widgets';
-import { ChartTitle, SmartChart } from './v1';
 
-const Chart = observer(({ show_digits_stats }: { show_digits_stats: boolean }) => {
-    const [view, setView] = useState('chart'); // Added state to manage the current view
-    const barriers: never[] = []; // Empty array for barriers
-    const { common, ui } = useStore();
-    const { chart_store, run_panel, dashboard } = useDBotStore();
+const Chart = observer(() => {
+    const [view, setView] = useState('tradingview'); // Added state to manage the current view
+    const { ui } = useStore();
+    const { run_panel, dashboard } = useDBotStore();
 
-    const {
-        chart_type,
-        getMarketsOrder,
-        granularity,
-        onSymbolChange,
-        setChartStatus,
-        symbol,
-        updateChartType,
-        updateGranularity,
-        wsForget,
-        wsForgetStream,
-        wsSendRequest,
-        wsSubscribe,
-    } = chart_store;
 
-    const { is_mobile, is_desktop } = ui;
+    const { is_mobile } = ui;
     const { is_drawer_open } = run_panel;
     const { is_chart_modal_visible } = dashboard;
-    const is_socket_opened = common.is_socket_opened;
 
-    const settings = {
-        assetInformation: false,
-        countdown: true,
-        isHighestLowestMarkerEnabled: false,
-        language: common.current_language.toLowerCase(),
-        position: ui.is_chart_layout_default ? 'bottom' : 'left',
-        theme: ui.is_dark_mode_on ? 'dark' : 'light',
-    };
 
     return (
         <div
